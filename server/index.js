92f0b69d5ff5b42fa7432d5099cf4f810011a790
Steve Warsa <swarsa480@gmail.com>
2020-05-31 10:15:44 -0700

    Created an Amazon docker run file and made deployment updates to the .travis.yml file

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+  const values = await pgClient.query(' * from values');
 
   res.send(values.rows);
 });
